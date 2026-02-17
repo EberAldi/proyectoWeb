cbc45e93a3422f5b79b02619371bd46b4924cbaa
Eber Aldair <ebernachito@gmail.com>
2026-02-16 22:54:21 -0600

    primer Commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-10 text-4xl space-x-4">
+    <font-awesome-icon icon="user" />
+    <font-awesome-icon icon="gamepad" />
+  </div>
+</template>
+<script setup> 
+  import { RouterView } from "vue-router";
+</script>
+  
+<template>  
+  <RouterView />
+</template>
